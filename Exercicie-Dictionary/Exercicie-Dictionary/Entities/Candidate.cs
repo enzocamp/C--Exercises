fb97534ff0a60473352930e92f1c800abd188cb3
Enzo Camp <enzo.flap@hotmail.com>
2024-08-05 20:37:54 -0300

    Exercicio de chave,valor com leitura de arquivo

---
+++ Exercicie-Dictionary/Exercicie-Dictionary/Entities/Candidate.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Exercicie_Dictionary.Entities
+{
+    internal class Candidate
+    {
+        public string Name { get; set; }
+        public int Votes { get; set; }
+
+        public Candidate(string name, int votes)
+        {
+            Name = name;
+            Votes = votes;
+        }
+    }
+}
---
+++ Exercicie-Dictionary/Exercicie-Dictionary/Program.cs
@@ -0,0 +1,68 @@
+﻿using Exercicie_Dictionary.Entities;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Exercicie_Dictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Dictionary<string, int> candidateVotes = new Dictionary<string, int>();
+
+            Console.WriteLine("Enter the full path:");
+            string url = Console.ReadLine();
+
+            if (url == null)
+            {
+                Console.WriteLine("O arquivo pode estar vazio ou não existe");
+            }
+            else
+            {
+
+                try
+                {
+                    using (StreamReader sr = File.OpenText(url))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+
+                            if (line != null)
+                            {
+                                string[] votes = line.Split(",");
+                                string name = votes[0];
+                                int vote = int.Parse(votes[1]);
+                                if (candidateVotes.ContainsKey(name))
+                                {
+                                    candidateVotes[name] += vote;
+                                }
+                                else
+                                {
+                                    candidateVotes[name] = vote;
+                                }
+                            }
+
+                        }
+                    }
+
+                    List<Candidate> candidates = new List<Candidate>();
+
+                    foreach (KeyValuePair<string, int> item in candidateVotes)
+                    {
+                        candidates.Add(new Candidate(item.Key, item.Value));
+                    }
+                    foreach (Candidate candidate in candidates)
+                    {
+                        Console.WriteLine($"{candidate.Name} : {candidate.Votes}");
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
